473057c7f8628afebdbdc4e92e8f88250221820f
aprilian tantra <tantraluhur@gmail.com>
2024-04-21 15:41:19 +0700

    feat: init material ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,31 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Lato, Roboto } from 'next/font/google';
+import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const lato = Lato({
+  weight: ["100", "300", "400", "700", "900"],
+  subsets: ["latin"]
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: lato.style.fontFamily,
+  },
+});
+
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <AppCacheProvider {...props}>
+      <Head>...</Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </AppCacheProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client';
+import { LoginSection } from "@/pages/components/login"
 
-const ws = new WebSocket("ws://127.0.0.1:8000/ws/1");
-export default function Home() {
-
-  const [messages, setMessages] = useState<any>([]);
-  const [newMessage, setNewMessage] = useState('');
-  
-
-  // Listen for messages
-  useEffect(() => {
-      ws.addEventListener("message", (event) => {
-        setMessages((prevMessages:any) => [...prevMessages, event.data]);
-      });
-  }, [])
-
-  const sendMessage = () => {
-    ws.send(newMessage)
-    setNewMessage('');
-  };
-
+export default function LandingPage() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
-    >
-    <div>
-      <h1>Real-Time Chat</h1>
-      <div>
-        {messages.map((message:any, index:any) => (
-          <div key={index}>{message}</div>
-        ))}
-      </div>
-      <input
-        type="text"
-        value={newMessage}
-        onChange={(e) => setNewMessage(e.target.value)}
-      />
-      <button onClick={sendMessage}>Send</button>
-    </div>
-    </main>
-  );
+      <LoginSection />
+  )
 }
