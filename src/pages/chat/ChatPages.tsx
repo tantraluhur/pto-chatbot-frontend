df04b9212b7d0285e0dba8228b0e5f2b7ce3cde0
aprilian tantra <tantraluhur@gmail.com>
2024-04-25 01:08:27 +0700

    feat: responsive web

--- src/pages/chat/ChatPages.tsx
+++ src/pages/chat/ChatPages.tsx
@@ -1,16 +1,21 @@
+import axios from "axios";
 import { ChatInput, QuestionBox } from "./components"
+import { useEffect } from "react";
 
 
 export const ChatSection = () => {
+
     return (
-        <div className="w-10/12 absolute bottom-0 m-8 grid gap-4">
-            <div className="grid grid-cols-2 gap-2">
-                <QuestionBox></QuestionBox>
-                <QuestionBox></QuestionBox>
-                <QuestionBox></QuestionBox>
-                <QuestionBox></QuestionBox>
+        <div className="flex justify-center items-center">
+            <div className="w-10/12 absolute bottom-0 mb-4 grid gap-4 sm:mb-8">
+                <div className="grid grid-cols-2 gap-2 sm:flex sm:justify-center sm:items-cen">
+                    <QuestionBox message="Bagaimana kategorisasi tingkat keamanan siber?"></QuestionBox>
+                    <QuestionBox message="Apa yang dimaksud dengan laporan insiden siber?"></QuestionBox>
+                    <QuestionBox message="Apa yang dimaksud dengan keamanan siber?"></QuestionBox>
+                    <QuestionBox message="Apa yang dilakukan Bank untuk menjaga ketahanan siber?"></QuestionBox>
+                </div>
+                <ChatInput></ChatInput>
             </div>
-            <ChatInput></ChatInput>
         </div>
     )
 }
--- src/pages/chat/components/QuestionBox.tsx
+++ src/pages/chat/components/QuestionBox.tsx
@@ -1,12 +1,15 @@
 import { useRef, useState, useEffect, ChangeEvent } from "react";
 
+type QuestionBoxProps = {
+    message: string
+}
 
-export const QuestionBox = () => {
+export const QuestionBox: React.FC<QuestionBoxProps> = ( {message} ) => {
     
     return (
         <div className="border-[1px] border-black rounded-2xl h-auto flex">
-            <div className="m-2 mr-8 text-sm">
-                Bagaimana kategorisasi tingkat keamanan siber?
+            <div className="m-2 mr-4 text-xs sm:mr-6 sm:text-sm">
+                {message}
             </div>
         </div>
     )
